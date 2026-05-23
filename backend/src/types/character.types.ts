52cbcd3c16eb0b49366eded101e872a046e12a8b
jjvp <vegajohan388@gmail.com>
2026-05-23 18:24:17 -0500

    Se introdujeron interfaces de TypeScript relacionadas con caracteres, como `Character`, `CharacterOrigin`, `ApiInfo`, `CharactersResponse`, `CharacterFilters` y `ApiError`.
    - Se añadió un archivo de configuración de TypeScript (`tsconfig.json`) para configurar el proyecto con comprobación de tipos estricta y características

---
+++ backend/src/types/character.types.ts
@@ -0,0 +1,43 @@
+export interface CharacterOrigin {
+  name: string;
+  url: string;
+}
+
+export interface Character {
+  id: number;
+  name: string;
+  status: 'Alive' | 'Dead' | 'unknown';
+  species: string;
+  type: string;
+  gender: 'Female' | 'Male' | 'Genderless' | 'unknown';
+  origin: CharacterOrigin;
+  location: CharacterOrigin;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+export interface ApiInfo {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+}
+
+export interface CharactersResponse {
+  info: ApiInfo;
+  results: Character[];
+}
+
+export interface CharacterFilters {
+  name?: string;
+  status?: string;
+  species?: string;
+  gender?: string;
+  page?: number;
+}
+
+export interface ApiError {
+  error: string;
+}
